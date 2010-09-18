3836457514fc2635f3cc2dd73f3c47661631529b
nadia senatore <nadiasenatore@ma-io-CHI.local>
2010-09-18 15:41:52 +0200

    Modifiche errore scala minore PitchClassDuration

--- BBMusicAnalysis.m
+++ BBMusicAnalysis.m
@@ -41,7 +41,7 @@ NSString* BBDimMode=@"d";
 
 
 unsigned int majorModePitchClasses[] = { 0, 2, 4, 5, 7, 9, 11};
-unsigned int minorModePitchClasses[] = { 0, 2, 4, 5, 7, 9, 11};
+unsigned int minorModePitchClasses[] = { 0, 2, 3, 5, 7, 8, 10};
 
 unsigned int* BBScalePitchClassesForChord(NSString* chord) {
 	NSString* mode = BBChordNameToMode(chord);
